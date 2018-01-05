a75c1edc311f108c4069066e2124ed27259936ea
Tim Z <zhenghanecho@gmail.com>
2018-01-05 17:30:53 +0800

    Build Login JS Auth

--- js/login.js
+++ js/login.js
@@ -1,24 +1,45 @@
 $(document).ready(function() {
     $("#login-btn").click(function() {
-        account_name = $("#account").val();
+    	//fake it till you make it.
+    	account_name = $("#account").val();
         password = $("#password").val();
+        if (account_name == "mofanlai" && password == "mofan123") {
+        	window.location = "portal.html"
+        }
+        else {
+        	$("#account").value = "";
+        	$("#password").value = "";
+        	alert("Authentication Failed, check your user name / password.");
+        }
 
-        $.ajax({
-            url: "login",
-            type: "POST",
-            data: {
-                usr: account_name,
-                pswd: password
-            },
-            dataType: "json",
-            contentType: "application/json",
-            success: function(data) {
-                console.log("Response " + data.accountSelected);
-            },
-            error: function( jqXhr, textStatus, errorThrown ){
-            	console.log( errorThrown );
-            	alert(textStatus + errorThrown);
-            }
-        });
+
+
+
+
+    	///Cross region request ERROR
+
+        // account_name = $("#account").val();
+        // password = $("#password").val();
+
+        // $.get("login").done(function (data) {console.log(data);});
+
+
+        // $.ajax({
+        //     url: "login",
+        //     type: "POST",
+        //     data: {
+        //         usr: account_name,
+        //         pswd: password
+        //     },
+        //     dataType: "json",
+        //     contentType: "application/json",
+        //     success: function(data) {
+        //         console.log("Response " + data.accountSelected);
+        //     },
+        //     error: function( jqXhr, textStatus, errorThrown ){
+        //     	// console.log( errorThrown );
+        //     	alert(textStatus + errorThrown);
+        //     }
+        // });
     })
 });
