0ca11e5d1feb72201b64fa805ca3a92ea1259144
Tim Z <zhenghanecho@gmail.com>
2018-01-06 23:09:40 +0800

    Build Dashboard

---
+++ portal/js/main.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+	$("#work-space").load("http://127.0.0.1/portal/dashboard.html");
+    $("#menu-dashboard").bind("click", () => {
+    	$("#work-space").load("http://127.0.0.1/portal/dashboard.html");
+    });
+    $("#menu-students").bind("click", () => {
+    });
+    $("#menu-database").bind("click", () => {
+    });
+    $("#menu-checklist").bind("click", () => {
+    });
+    $("#menu-memo").bind("click", () => {
+    });
+    $("#menu-settings").bind("click", () => {
+    });
+    $("#menu-support").bind("click", () => {
+    });
+
+});
