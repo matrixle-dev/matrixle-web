68986248fc49d15c55eec5ce8bfbae68d595ea32
Tim Z <zhenghanecho@gmail.com>
2018-01-10 15:29:29 +0800

    Test Code

--- portal/js/main.js
+++ portal/js/main.js
@@ -1,19 +1,32 @@
 $(document).ready(function() {
-	$("#work-space").load("http://timzh.me/portal/dashboard.html");
+    $("#work-space").load("http://timzh.me/portal/dashboard.html");
     $("#menu-dashboard").bind("click", () => {
-    	$("#work-space").load("http://timzh.me/portal/dashboard.html");//http://timzh.me/portal/
+        $("#work-space").load("http://timzh.me/portal/dashboard.html"); //http://timzh.me/portal/
     });
     $("#menu-students").bind("click", () => {
+            console.log("");
+            var myUrl = "search/";
+
+            $.ajax({
+                url: myUrl,
+                type: "POST",
+                data: "{message="hello"} ,
+                dataType: "json",
+                contentType: "application/json",
+                success: function(result) {
+                    console.log("Response " + JSON.stringify(result));
+                },
+                error: function(request, textStatus, errorThrown) {
+                    alert('textStatus ' + textStatus);
+                    alert('errorThrown ' + errorThrown);
+                }
+            });
+        }
     });
-    $("#menu-database").bind("click", () => {
-    });
-    $("#menu-checklist").bind("click", () => {
-    });
-    $("#menu-memo").bind("click", () => {
-    });
-    $("#menu-settings").bind("click", () => {
-    });
-    $("#menu-support").bind("click", () => {
-    });
+    $("#menu-database").bind("click", () => {});
+    $("#menu-checklist").bind("click", () => {});
+    $("#menu-memo").bind("click", () => {});
+    $("#menu-settings").bind("click", () => {});
+    $("#menu-support").bind("click", () => {});
 
 });
