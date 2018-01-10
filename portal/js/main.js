c970250d5170a0a8e75c842e1ec02e3552002415
Tim Z <zhenghanecho@gmail.com>
2018-01-10 15:44:08 +0800

    Fixed Bug

--- portal/js/main.js
+++ portal/js/main.js
@@ -4,24 +4,24 @@ $(document).ready(function() {
         $("#work-space").load("http://timzh.me/portal/dashboard.html"); //http://timzh.me/portal/
     });
     $("#menu-students").bind("click", () => {
-            var myUrl = "search/";
+        var myUrl = "search/";
 
-            $.ajax({
-                url: myUrl,
-                type: "POST",
-                data: "{message=10000}",
-                dataType: "json",
-                contentType: "application/json",
-                success: function(result) {
-                    console.log("Response " + JSON.stringify(result));
-                },
-                error: function(request, textStatus, errorThrown) {
-                    alert('textStatus ' + textStatus);
-                    alert('errorThrown ' + errorThrown);
-                }
-            });
-        }
-    }); $("#menu-database").bind("click", () => {
+        $.ajax({
+            url: myUrl,
+            type: "POST",
+            data: "{message=10000}",
+            dataType: "json",
+            contentType: "application/json",
+            success: function(result) {
+                console.log("Response " + JSON.stringify(result));
+            },
+            error: function(request, textStatus, errorThrown) {
+                alert('textStatus ' + textStatus);
+                alert('errorThrown ' + errorThrown);
+            }
+        });
+    });
+    $("#menu-database").bind("click", () => {
         var myUrl = "search/";
 
         $.ajax({
@@ -38,47 +38,44 @@ $(document).ready(function() {
                 alert('errorThrown ' + errorThrown);
             }
         });
-    }
-});
-$("#menu-checklist").bind("click", () => {
-    var myUrl = "http://timzh.me:4444";
-
-    $.ajax({
-        url: myUrl,
-        type: "POST",
-        data: "{message=10000}",
-        dataType: "json",
-        contentType: "application/json",
-        success: function(result) {
-            console.log("Response " + JSON.stringify(result));
-        },
-        error: function(request, textStatus, errorThrown) {
-            alert('textStatus ' + textStatus);
-            alert('errorThrown ' + errorThrown);
-        }
     });
-}
-});
-$("#menu-memo").bind("click", () => {
-    var myUrl = "http://timzh.me:4444";
+    $("#menu-checklist").bind("click", () => {
+        var myUrl = "http://timzh.me:4444";
 
-    $.ajax({
-        url: myUrl,
-        type: "GET",
-        data: "{message=10000}",
-        dataType: "json",
-        contentType: "application/json",
-        success: function(result) {
-            console.log("Response " + JSON.stringify(result));
-        },
-        error: function(request, textStatus, errorThrown) {
-            alert('textStatus ' + textStatus);
-            alert('errorThrown ' + errorThrown);
-        }
+        $.ajax({
+            url: myUrl,
+            type: "POST",
+            data: "{message=10000}",
+            dataType: "json",
+            contentType: "application/json",
+            success: function(result) {
+                console.log("Response " + JSON.stringify(result));
+            },
+            error: function(request, textStatus, errorThrown) {
+                alert('textStatus ' + textStatus);
+                alert('errorThrown ' + errorThrown);
+            }
+        });
     });
-}
-});
-$("#menu-settings").bind("click", () => {});
-$("#menu-support").bind("click", () => {});
+    $("#menu-memo").bind("click", () => {
+        var myUrl = "http://timzh.me:4444";
+
+        $.ajax({
+            url: myUrl,
+            type: "GET",
+            data: "{message=10000}",
+            dataType: "json",
+            contentType: "application/json",
+            success: function(result) {
+                console.log("Response " + JSON.stringify(result));
+            },
+            error: function(request, textStatus, errorThrown) {
+                alert('textStatus ' + textStatus);
+                alert('errorThrown ' + errorThrown);
+            }
+        });
+    });
+    $("#menu-settings").bind("click", () => {});
+    $("#menu-support").bind("click", () => {});
 
 });
