2710fc4ea766566e4e3138a733aafa85c2f96e37
Tim Z <zhenghanecho@gmail.com>
2018-03-18 21:02:46 -0400

    Update

--- js/login.js
+++ js/login.js
@@ -2,40 +2,33 @@ $(document).ready(function() {
     $("#login-btn").click(function() {
         account_name = $("#account").val();
         password = $("#password").val();
-    	
+
         //// fake it till you make it.
         if (account_name == "mofanlai" && password == "mofan123") {
-        	window.location = "./portal/portal.html"
-        }
-        else {
-        	$("#account").value = "";
-        	$("#password").value = "";
-        	alert("Authentication Failed, check your user name / password.");
+            window.location = "./portal/portal.html"
+        } else {
         }
 
-
-
-
-
-    	//Cross region request ERROR
-        // $.get("login").done(function (data) {console.log(data);});
-
-
+        var myUrl = "https://matrixle.com/login";
         $.ajax({
-            url: "http://matrixle.com:4444",
-            type: "POST",
-            data: {
-                usr: account_name,
-                pswd: password
-            },
+            url: myUrl,
+            type: "GET",
+            data: JSON.stringify({ user_name: account_name, passwd: password}),
             dataType: "json",
             contentType: "application/json",
-            success: function(data) {
-                console.log("Response " + data.accountSelected);
+            success: function(result) {
+                console.log("Response " + JSON.stringify(result));
+                if(JSON.stringify(result) == '{"result": true}'){
+                    window.location = "./portal/portal.html"
+                } else {
+                    $("#account").value = "";
+                    $("#password").value = "";
+                    alert("Authentication Failed, check your user name / password.");
+                }
             },
-            error: function( jqXhr, textStatus, errorThrown ){
-            	// console.log( errorThrown );
-            	// alert(textStatus + errorThrown);
+            error: function(request, textStatus, errorThrown) {
+                alert('textStatus ' + textStatus);
+                alert('errorThrown ' + errorThrown);
             }
         });
     })
